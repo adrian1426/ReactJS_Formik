42338eb1e82f179f2a26c0cebfb9ee527f683765
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2021-11-13 17:43:12 -0600

    Intruducción a las validaciones en formik

--- src/App.js
+++ src/App.js
@@ -6,10 +6,35 @@ const initialState = {
   email: ''
 };
 
+const validate = values => {
+  const errors = {};
+
+  if (!values.nombre) {
+    errors.nombre = 'requerido';
+  } else if (values.nombre.length < 5) {
+    errors.nombre = 'el nombre es muy corto';
+  }
+
+  if (!values.apellido) {
+    errors.apellido = 'requerido';
+  } else if (values.apellido.length < 5) {
+    errors.apellido = 'el apellido es muy corto';
+  }
+
+  if (!values.email) {
+    errors.email = 'requerido';
+  } else if (values.email.length < 5) {
+    errors.email = 'el email es muy corto';
+  }
+
+  return errors;
+};
+
 function App() {
   const formik = useFormik({
     initialValues: initialState,
-    onSubmit: values => console.log('values: ', values)
+    onSubmit: values => console.log('values: ', values),
+    validate
   });
 
   return (
@@ -21,6 +46,9 @@ function App() {
         onChange={formik.handleChange}
         value={formik.values.nombre}
       />
+      {formik.errors.nombre && <div>{formik.errors.nombre}</div>}
+
+      <br />
 
       <label>Apellido</label>
       <input
@@ -29,6 +57,9 @@ function App() {
         onChange={formik.handleChange}
         value={formik.values.apellido}
       />
+      {formik.errors.apellido && <div>{formik.errors.apellido}</div>}
+
+      <br />
 
       <label>Correo</label>
       <input
@@ -37,6 +68,7 @@ function App() {
         onChange={formik.handleChange}
         value={formik.values.email}
       />
+      {formik.errors.email && <div>{formik.errors.email}</div>}
 
       <button type="submit">Enviar</button>
     </form>
